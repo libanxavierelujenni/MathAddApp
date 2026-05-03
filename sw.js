44017b480230c28370d1f137a825dfeacea65721
X <x@MacBookPro.lan>
2026-05-03 23:32:36 +1000

    chore: add PWA requirements (icons and service worker) for Android deployment

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'math-dash-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './index.css',
+  './app.js',
+  './manifest.json',
+  './icon.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
